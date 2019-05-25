137b5187fa7a1ceac9ad9b041e0e52d1960e44c0
Konrad Eisele <eiselekd@gmail.com>
2019-05-25 21:56:02 +0200

    all

--- files/code.js
+++ files/code.js
@@ -1,8 +1,20 @@
+function viewOnlyChanged() {
+    var a = $("#d2_a");
+}
 
 function gen_tree(n) {
     this.c = [];
     this.n = n;
-    this.e = {};
+    this.e = {'color':0,'attr':{'class':[]}};
+    this.color = 0;
+}
+
+function ismember(a,n) {
+    for (var i in a) {
+        if (a[i] == n)
+            return 1;
+    }
+    return 0;
 }
 
 gen_tree.prototype.gen = function(na,e) {
@@ -17,9 +29,19 @@ gen_tree.prototype.gen = function(na,e) {
         function(a, b) { return ('' + a.n).localeCompare(b.n); });
     if (_n.length != 0) {
         c.gen(_n,e);
+        c.e.color |= e.color;
     } else {
         c.e = e;
     }
+}
+
+function converthexcolor(a) {
+    var k = ""; var i;
+    for (i = 0; i < 6;  i++ ) {
+        var j = a & 0xf; a >>= 4;
+        k = j.toString(16)+k;
+    }
+    return k;
 }
 
 gen_tree.prototype.html = function(na) {
@@ -40,9 +62,29 @@ gen_tree.prototype.html = function(na) {
         arg0 = this.e['path'];
         a = a.concat(toadd);
     }
+    var col = "";
+    if (this.e.color && !ismember(this.e.attr.class,'file')) {
+        col = 0xffffff;
+        if (this.e.color & 0x2) { // grey
+            col -= 0x101010;
+        } else if (this.e.color & 0x4) { // green
+            col -= 0x300030;
+        } else if (this.e.color & 0x1) { // red
+            col -= 0x003030;
+        }
+        col = converthexcolor(col);
+        console.log(col);
+        col = "background-color:#"+col;
+    }
+
     var l = c.join("\n");
+    var id = "";
+    if ('path' in this.e) {
+        id = this.e['path'];
+        id = id.replace(/[\/\s\.@]/ig, "_");
+    }
     var args = [arg0, arg1].map(function(a) { return "\""+a+"\""; }).join(",");
-    return "<li><span class=\""+a.join(" ")+"\"><a onclick='"+func+"("+args+")' >" + this.n + "</a></span><ul> " + l + "</ul></li>";
+    return "<li><span class=\""+a.join(" ")+"\"><a style=\""+col+"\" id=\""+id+"\" onclick='"+func+"("+args+")' >" + this.n + "</a></span><ul> " + l + "</ul></li>";
 }
 
 function init_repo_tree(b,a) {
@@ -69,11 +111,33 @@ function init_diff_tree(b,a) {
     var treear = new gen_tree('root')
     for (var v in a)
     {
-        na = a[v]['path'].split("/");
-        treear.gen(na, a[v]);
+        var e = a[v];
+        na = e['path'].split("/");
+        e.color = 0;
+        if (ismember(e.attr.class,'diffnew')) {
+            e.color = 0x4;
+        } else if (ismember(e.attr.class,'diffremainchanged')) {
+            e.color = 0x2;
+        } else if (ismember(e.attr.class,'diffremoved')) {
+            e.color = 0x1;
+        }
+        treear.gen(na, e);
     }
     var p = treear.html();
     $(b).append(p);
 
+    $(".diffnew").each(function(i,e) {
+        propagate(e,0,0x10,0);
+    });
+    $(".diffremoved").each(function(i,e) {
+        propagate(e,0x10,0,0);
+    });
+    $(".diffremainchanged").each(function(i,e) {
+        propagate(e,0x10,0x10,0x10);
+    });
     console.log(treear)
+}
+
+function propagate(e,r,g,b) {
+
 }
