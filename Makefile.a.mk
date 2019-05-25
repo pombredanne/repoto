eaf41c2c8f083a6032bd8ae7f0c5392fa3cc04e4
Konrad Eisele <eiselekd@gmail.com>
2019-05-26 01:37:49 +0200

    all

---
+++ Makefile.a.mk
@@ -0,0 +1,54 @@
+include Makefile.config.mk
+
+SHELL=bash
+
+all:
+
+aosp:
+	echo "lineage needs to be prepared"
+	-rm /tmp/kati.log
+	mkdir -p $(CURDIR)/$(DATE)
+	cd kati; make
+	if [ -d $(AOSP_BASE)/build/kati ]; then \
+		cp kati/*cc $(AOSP_BASE)/build/kati/;  \
+		cp kati/*h $(AOSP_BASE)/build/kati/;  \
+		cp kati/Makefile* $(AOSP_BASE)/build/kati/;  \
+	fi
+	-cp kati/ckati $(AOSP_BASE)/prebuilts/build-tools/linux-x86/bin/ckati
+	-cp kati/ckati $(AOSP_BASE)/out/host/linux-x86/bin/ckati
+	touch $(AOSP_TOUCH)
+	cd $(AOSP_BASE); source build/envsetup.sh; lunch $(AOSP_LUNCH);  \
+		$(CURDIR)/monitor.sh make showcommands 2>&1 | tee $(CURDIR)/$(DATE)/compile_kati.txt
+
+aosp-process:
+	cat /tmp/kati.log  | grep -ia LOAD-file > kati_$(AOSP_LUNCH)_list_filter.txt
+	perl first_kati.pl kati_$(AOSP_LUNCH)_list_filter.txt kati_$(AOSP_LUNCH)_list_first.txt
+	perl last_kati.pl kati_$(AOSP_LUNCH)_list_filter.txt kati_$(AOSP_LUNCH)_list_last.txt
+
+aosp-process-config:
+	#-rm -rf kati_lineage_config-out
+	mkdir -p kati_$(AOSP_LUNCH)_config-out
+	/usr/bin/perl kati-dep-tree.pl \
+		--lineage $(AOSP_BASE) \
+		--base build/core/config.mk \
+		--out kati_$(AOSP_LUNCH)_config-out  \
+		kati_$(AOSP_LUNCH)_list_first.txt index.html
+	google-chrome --allow-file-access-from-files kati_$(AOSP_LUNCH)_config-out/index.html
+
+aosp-process-main:
+	#-rm -rf kati_lineage_main-out
+	mkdir -p kati_$(AOSP_LUNCH)_main-out
+	/usr/bin/perl kati-dep-tree.pl \
+		--lineage $(AOSP_BASE) \
+		--base build/core/main.mk \
+		--out kati_$(AOSP_LUNCH)_main-out  \
+		kati_$(AOSP_LUNCH)_list_last.txt index.html
+	google-chrome --allow-file-access-from-files kati_$(AOSP_LUNCH)_main-out/index.html
+
+aosp-process-dep:
+	mkdir -p kati_$(AOSP_LUNCH)_main-out
+	/usr/bin/perl kati-dep.pl \
+		--lineage $(AOSP_BASE) \
+		--base build/core/config.mk \
+		--out kati_$(AOSP_LUNCH)_main-out  \
+		kati_$(AOSP_LUNCH)_list_last.txt index.html
---
+++ kati-dep.pl
@@ -0,0 +1,52 @@
+#!/usr/bin/perl
+use Data::Dumper;
+use Getopt::Long;
+use File::Basename;
+use File::Path;
+use FindBin qw($Bin);
+use JSON::MaybeXS qw(encode_json decode_json);
+use Cwd;
+use Carp;
+use Cwd 'abs_path';
+require "$Bin/templ.pm";
+require "$Bin/kati.pm";
+
+Getopt::Long::Configure(qw(bundling));
+GetOptions(\%OPT,qw{
+    quiet|q+
+    verbose|v+
+    lineage=s
+    base=s
+    out=s
+  } ,@g_more) or usage(\*STDERR);
+  
+  $lineage=$OPT{'lineage'};
+  $base=$OPT{'base'};
+  $outdir=$OPT{'out'};
+
+
+my $filename = $ARGV[0];
+open(my $fh, '<:encoding(UTF-8)', $filename)
+  or die "Could not open file '$filename' $!";
+
+%defines=();
+%stackmap=();
+%assign=();
+%ctxmap=();
+
+while(<$fh>) {
+    if (/LOAD-file-proj-assign: (ALL_DEPS[\[\]A-Za-z\@0-9_\-\/\.\+]+)=<\{(.*)\}> :(.*)/) {
+	my ($defname,$evalstk,$content) = ($1,$2,$3);
+	$assign{$defname} = [] if (!exists($assign{$defname}));
+	push(@{$assign{$defname}},
+	     new kati::assign($evalstk,$content));
+	print($content) if ($OPT{'verbose'});
+	if (!($defname eq "ALL_DEPS.MODULES")) {
+	    print(": $defname=$content\n") ;
+	}
+    } else {
+	#print("Error :: '$_'\n");
+	#exit(1);
+    }
+}
+
